3d3f8e38b9ee91fad46d58ea10bce2171268427a
navneeth-official <jnavneethkrishnan@gmail.com>
2026-08-31 15:51:50 +0530

    Test GitLab deploy trigger

--- DSR-WSR-AUTOMATION-Frontend/Frontend/src/main.tsx
+++ DSR-WSR-AUTOMATION-Frontend/Frontend/src/main.tsx
@@ -6,5 +6,5 @@
   createRoot(document.getElementById("root")!).render(<App />);
 
 
-//  Makeing few lines of code chnages to check gitlabs pipeline trigger working 
+//  Makeing few lines of code chnages to check gitlabs pipeline trigger
   
